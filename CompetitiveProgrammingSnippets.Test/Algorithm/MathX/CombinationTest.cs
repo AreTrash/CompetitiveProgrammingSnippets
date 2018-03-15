ce61d010b6d35f47c873f014082469f8d073bd10
AreTrash <tinpuny44@gmail.com>
2018-03-15 17:16:43 +0900

    test: combination 強化

--- CompetitiveProgrammingSnippets.Test/Algorithm/MathX/CombinationTest.cs
+++ CompetitiveProgrammingSnippets.Test/Algorithm/MathX/CombinationTest.cs
@@ -29,9 +29,12 @@ namespace Algorithm.MathX
             Assert.Equal(1, comb.NCK(1, 1));
             Assert.Equal(0, comb.NCK(1, 2));
 
+            Assert.Equal(1, comb.NCK(4, 0));
             Assert.Equal(4, comb.NCK(4, 1));
             Assert.Equal(6, comb.NCK(4, 2));
             Assert.Equal(4, comb.NCK(4, 3));
+            Assert.Equal(1, comb.NCK(4, 4));
+            Assert.Equal(0, comb.NCK(4, 5));
 
             Assert.Equal(538992043, comb.NCK(100, 50));
         }
@@ -44,9 +47,12 @@ namespace Algorithm.MathX
             Assert.Equal(1, comb.NPK(1, 1));
             Assert.Equal(0, comb.NPK(1, 2));
 
+            Assert.Equal(1, comb.NPK(4, 0));
             Assert.Equal(4, comb.NPK(4, 1));
             Assert.Equal(12, comb.NPK(4, 2));
             Assert.Equal(24, comb.NPK(4, 3));
+            Assert.Equal(24, comb.NPK(4, 4));
+            Assert.Equal(0, comb.NPK(4, 5));
 
             Assert.Equal(505671657, comb.NPK(100, 50));
         }
@@ -59,6 +65,7 @@ namespace Algorithm.MathX
             Assert.Equal(1, comb.NHK(1, 1));
             Assert.Equal(1, comb.NHK(1, 2));
 
+            Assert.Equal(1, comb.NHK(4, 0));
             Assert.Equal(4, comb.NHK(4, 1));
             Assert.Equal(10, comb.NHK(4, 2));
             Assert.Equal(20, comb.NHK(4, 3));
