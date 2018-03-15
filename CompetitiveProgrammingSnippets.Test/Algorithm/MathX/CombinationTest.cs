0b68ead5aad0d0ab1f4c1269f3e698ed92e06836
AreTrash <tinpuny44@gmail.com>
2018-03-15 18:21:11 +0900

    fix: combination: rv

--- CompetitiveProgrammingSnippets.Test/Algorithm/MathX/CombinationTest.cs
+++ CompetitiveProgrammingSnippets.Test/Algorithm/MathX/CombinationTest.cs
@@ -4,10 +4,16 @@ namespace Algorithm.MathX
 {
     public class CombinationTest
     {
+        readonly Combination comb;
+
+        public CombinationTest()
+        {
+            this.comb = new Combination(200);
+        }
+
         [Fact]
         void Fact()
         {
-            var comb = new Combination(200);
             Assert.Equal(1, comb.Fact(0));
             Assert.Equal(1, comb.Fact(1));
             Assert.Equal(2, comb.Fact(2));
@@ -18,8 +24,6 @@ namespace Algorithm.MathX
         [Fact]
         void NCK()
         {
-            var comb = new Combination(200);
-            Assert.Equal(1, comb.NCK(0, 0));
             Assert.Equal(0, comb.NCK(0, 1));
             Assert.Equal(1, comb.NCK(1, 0));
             Assert.Equal(1, comb.NCK(1, 1));
@@ -38,8 +42,6 @@ namespace Algorithm.MathX
         [Fact]
         void NPK()
         {
-            var comb = new Combination(200);
-            Assert.Equal(1, comb.NPK(0, 0));
             Assert.Equal(0, comb.NPK(0, 1));
             Assert.Equal(1, comb.NPK(1, 0));
             Assert.Equal(1, comb.NPK(1, 1));
@@ -58,8 +60,6 @@ namespace Algorithm.MathX
         [Fact]
         void NHK()
         {
-            var comb = new Combination(200);
-            Assert.Equal(1, comb.NHK(0, 0));
             Assert.Equal(0, comb.NHK(0, 1));
             Assert.Equal(1, comb.NHK(1, 0));
             Assert.Equal(1, comb.NHK(1, 1));
