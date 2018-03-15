eb37c21fe4ab42d1b347af175f05d0f80153dbe1
AreTrash <tinpuny44@gmail.com>
2018-03-16 01:46:43 +0900

    feat: prime

---
+++ CompetitiveProgrammingSnippets.Test/Algorithm/MathX/PrimeTest.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Algorithm.MathX
+{
+    public class PrimeTest
+    {
+        readonly Prime prime;
+
+        public PrimeTest()
+        {
+            prime = new Prime(100);
+        }
+
+        [Fact]
+        void Construct()
+        {
+            Assert.Equal(new[]{2, 3, 5, 7}, new Prime(10).List);
+            Assert.Equal(new[]{2, 3, 5, 7, 11}, new Prime(11).List);
+        }
+
+        [Fact]
+        void Is()
+        {
+            Assert.False(prime.Is(-1));
+            Assert.False(prime.Is(0));
+            Assert.False(prime.Is(1));
+
+            Assert.True(prime.Is(2));
+            Assert.True(prime.Is(3));
+            Assert.False(prime.Is(4));
+
+            Assert.False(prime.Is(57));
+
+            Assert.False(prime.Is(99));
+            Assert.False(prime.Is(100));
+            Assert.True(prime.Is(101));
+
+            Assert.True(prime.Is(9973));
+            Assert.False(prime.Is(9999));
+            Assert.False(prime.Is(10000));
+
+            //n <= max ^ 2 じゃないと駄目
+            Assert.True(prime.Is(101 * 101));
+        }
+
+        [Fact]
+        void Factorize()
+        {
+            Assert.Equal(new int[0], prime.Factorize(1));
+
+            Assert.Equal(new[] {2}, prime.Factorize(2));
+            Assert.Equal(new[] {3}, prime.Factorize(3));
+            Assert.Equal(new[] {2, 2}, prime.Factorize(4));
+
+            Assert.Equal(new[] {3, 19}, prime.Factorize(57));
+            Assert.Equal(new[] {2, 2, 5, 5}, prime.Factorize(100));
+            Assert.Equal(new[] {2, 2, 2, 3, 3, 5}, prime.Factorize(360));
+
+            Assert.Equal(new[] {9973}, prime.Factorize(9973));
+            Assert.Equal(new[] {3, 3, 11, 101}, prime.Factorize(9999));
+            Assert.Equal(new[] {2, 2, 2, 2, 5, 5, 5, 5}, prime.Factorize(10000));
+
+            //n <= max ^ 2 じゃないと駄目
+            Assert.Equal(new[] {10201}, prime.Factorize(101 * 101));
+        }
+    }
+}
---
+++ CompetitiveProgrammingSnippets/Algorithm/MathX/Prime.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithm.MathX
+{
+    using __int__ = Int32;
+
+    //$prime
+    //@Prime 素数列挙(2 ~ max), 素数判定(n <= max ^ 2), 因数分解(1 <= n <= max ^ 2)
+    public class Prime
+    {
+        readonly List<__int__> primes;
+        public IReadOnlyList<__int__> List { get { return primes; } }
+
+        public Prime(int max)
+        {
+            primes = new List<__int__>();
+
+            var sieve = new bool[max + 1];
+            for (var i = 2; i <= max; i++)
+            {
+                if (sieve[i]) continue;
+                primes.Add(i);
+                for (var j = i * 2; j <= max; j += i) sieve[j] = true;
+            }
+        }
+
+        public bool Is(__int__ n)
+        {
+            return n > 1 && primes.TakeWhile(p => p * p <= n).All(p => n % p != 0);
+        }
+
+        public IEnumerable<__int__> Factorize(__int__ n)
+        {
+            foreach (var p in primes.TakeWhile(p => p * p <= n))
+            {
+                while (n % p == 0)
+                {
+                    n /= p;
+                    yield return p;
+                }
+            }
+            if (n != 1) yield return n;
+        }
+    }
+    //$prime
+}
