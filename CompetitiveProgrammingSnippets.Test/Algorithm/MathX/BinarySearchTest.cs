ce8bbf7c5315a27821965259897cb20ed26593b9
AreTrash <tinpuny44@gmail.com>
2019-01-16 16:21:13 +0900

    feat: BinarySearch

---
+++ CompetitiveProgrammingSnippets.Test/Algorithm/MathX/BinarySearchTest.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using Xunit;
+
+namespace Algorithm.MathX
+{
+    public class BinarySearchTest
+    {
+        [Fact]
+        void Exist()
+        {
+            var inc = new long[] {1, 5, 6, 7, 9};
+            Assert.Equal(0, inc.OutLeft(5));
+            Assert.Equal(1, inc.InLeft(5));
+            Assert.Equal(1, inc.InRight(5));
+            Assert.Equal(2, inc.OutRight(5));
+
+            var dec = new long[] {9, 5, 3, 2, 1};
+            Assert.Equal(0, dec.OutLeft(5));
+            Assert.Equal(1, dec.InLeft(5));
+            Assert.Equal(1, dec.InRight(5));
+            Assert.Equal(2, dec.OutRight(5));
+        }
+
+        [Fact]
+        void ExistMultiple()
+        {
+            var a = new long[] {1, 5, 5, 5, 9};
+
+            Assert.Equal(0, a.OutLeft(5));
+            Assert.Equal(1, a.InLeft(5));
+            Assert.Equal(3, a.InRight(5));
+            Assert.Equal(4, a.OutRight(5));
+        }
+
+        [Fact]
+        void ExistFirst()
+        {
+            var a = new long[] {5, 6, 7, 8, 9};
+
+            Assert.Equal(-1, a.OutLeft(5));
+            Assert.Equal(0, a.InLeft(5));
+            Assert.Equal(0, a.InRight(5));
+            Assert.Equal(1, a.OutRight(5));
+        }
+
+        [Fact]
+        void ExistLast()
+        {
+            var a = new long[] {1, 2, 3, 4, 5};
+
+            Assert.Equal(3, a.OutLeft(5));
+            Assert.Equal(4, a.InLeft(5));
+            Assert.Equal(4, a.InRight(5));
+            Assert.Equal(5, a.OutRight(5));
+        }
+
+        [Fact]
+        void InsideButNotExist()
+        {
+            var a = new long[] {0, 2, 4, 6, 8};
+
+            Assert.Equal(2, a.OutLeft(5));
+            Assert.Equal(2, a.InLeft(5));
+            Assert.Equal(3, a.InRight(5));
+            Assert.Equal(3, a.OutRight(5));
+        }
+
+        [Fact]
+        void OutOfLeft()
+        {
+            var a = new long[] {6, 7, 8, 9, 10};
+
+            Assert.Equal(-1, a.OutLeft(5));
+            Assert.Equal(-1, a.InLeft(5));
+            Assert.Equal(-1, a.InRight(5));
+            Assert.Equal(-1, a.OutRight(5));
+        }
+
+        [Fact]
+        void OutOfRight()
+        {
+            var a = new long[] {0, 1, 2, 3, 4};
+
+            Assert.Equal(5, a.OutLeft(5));
+            Assert.Equal(5, a.InLeft(5));
+            Assert.Equal(5, a.InRight(5));
+            Assert.Equal(5, a.OutRight(5));
+        }
+
+        [Fact]
+        void Phi()
+        {
+            var inc = new long[0];
+
+            Assert.Throws<ArgumentException>(() => inc.InLeft(5));
+            Assert.Throws<ArgumentException>(() => inc.OutLeft(5));
+            Assert.Throws<ArgumentException>(() => inc.InRight(5));
+            Assert.Throws<ArgumentException>(() => inc.OutRight(5));
+        }
+
+        [Fact]
+        void Single()
+        {
+            var a = new long[] {5};
+
+            Assert.Equal(-1, a.OutLeft(5));
+            Assert.Equal(0, a.InLeft(5));
+            Assert.Equal(0, a.InRight(5));
+            Assert.Equal(1, a.OutRight(5));
+        }
+
+        [Fact]
+        void Double()
+        {
+            var a = new long[] {0, 10};
+
+            Assert.Equal(0, a.OutLeft(5));
+            Assert.Equal(0, a.InLeft(5));
+            Assert.Equal(1, a.InRight(5));
+            Assert.Equal(1, a.OutRight(5));
+        }
+
+        [Fact]
+        void AllSame()
+        {
+            var a = new long[] {5, 5, 5, 5, 5};
+
+            Assert.Equal(-1, a.OutLeft(5));
+            Assert.Equal(0, a.InLeft(5));
+            Assert.Equal(4, a.InRight(5));
+            Assert.Equal(5, a.OutRight(5));
+        }
+
+        [Fact]
+        void Range()
+        {
+            Assert.Equal(3, new long[] {0, 1, 2, 3, 4}.Range(1, 3));
+            Assert.Equal(4, new long[] {0, 1, 1, 1, 3, 4}.Range(1, 3));
+            Assert.Equal(4, new long[] {0, 1, 1, 3, 3, 4}.Range(1, 3));
+            Assert.Equal(4, new long[] {0, 1, 3, 3, 3, 4}.Range(1, 3));
+            Assert.Equal(2, new long[] {0, 1, 3}.Range(1, 3));
+            Assert.Equal(2, new long[] {1, 3, 4}.Range(1, 3));
+            Assert.Equal(4, new long[] {0, 2, 2, 2, 3}.Range(1, 3));
+            Assert.Equal(4, new long[] {1, 2, 2, 2, 4}.Range(1, 3));
+            Assert.Equal(3, new long[] {2, 2, 2}.Range(1, 3));
+            Assert.Equal(1, new long[] {0, 1}.Range(1, 3));
+            Assert.Equal(1, new long[] {3, 4}.Range(1, 3));
+            Assert.Equal(0, new long[] {0}.Range(1, 3));
+            Assert.Equal(1, new long[] {1}.Range(1, 3));
+            Assert.Equal(1, new long[] {2}.Range(1, 3));
+            Assert.Equal(1, new long[] {3}.Range(1, 3));
+            Assert.Equal(0, new long[] {4}.Range(1, 3));
+        }
+
+        [Fact]
+        void FuncSearch()
+        {
+            Func<int, long> func = x => x * x;
+
+            Assert.Equal(4, BinarySearch.OutLeft(func, 25, 0, 20));
+            Assert.Equal(5, BinarySearch.InLeft(func, 25, 0, 20));
+            Assert.Equal(5, BinarySearch.InRight(func, 25, 0, 20));
+            Assert.Equal(6, BinarySearch.OutRight(func, 25, 0, 20));
+
+            Assert.Equal(0, BinarySearch.Range(func, -10, -5, 0, 20));
+            Assert.Equal(1, BinarySearch.Range(func, -10, 0, 0, 20));
+            Assert.Equal(5, BinarySearch.Range(func, -10, 20, 0, 20));
+            Assert.Equal(6, BinarySearch.Range(func, 20, 100, 0, 20));
+            Assert.Equal(11, BinarySearch.Range(func, 100, 400, 0, 20));
+            Assert.Equal(1, BinarySearch.Range(func, 400, 114514, 0, 20));
+            Assert.Equal(21, BinarySearch.Range(func, -114514, 114514, 0, 20));
+            Assert.Equal(20, BinarySearch.Range(func, -74977, 399, 0, 20));
+        }
+    }
+}
---
+++ CompetitiveProgrammingSnippets/Algorithm/MathX/BinarySearch.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithm.MathX
+{
+    using __int__ = Int32;
+    using __long__ = Int64;
+
+    //$bs
+    //@二分探索
+    public static partial class BinarySearch
+    {
+        static __int__ Left(Func<__int__, bool> pred, __int__ left, __int__ right)
+        {
+            if (left > right) throw new ArgumentException("left must <= right");
+
+            while (true)
+            {
+                if (left == right) break;
+                var mid = (left + right + 1) / 2;
+                if (pred(mid)) left = mid;
+                else right = mid - 1;
+            }
+
+            return right;
+        }
+
+        static __int__ Right(Func<__int__, bool> pred, __int__ left, __int__ right)
+        {
+            if (left > right) throw new ArgumentException("left must <= right");
+
+            while (true)
+            {
+                if (left == right) return left;
+                var mid = (left + right - 1) / 2;
+                if (pred(mid)) right = mid;
+                else left = mid + 1;
+            }
+        }
+
+        enum SearchType
+        {
+            OutLeft, InLeft, InRight, OutRight
+        }
+
+        static __int__ Search(SearchType st, Func<__int__, __long__> func, __long__ value, __int__ min, __int__ max)
+        {
+            var isInc = func(min) <= func(max);
+
+            //TODO InLeft, InRight
+            if (st == SearchType.OutLeft && isInc) return Left(x => func(x) < value, min, max);
+            if (st == SearchType.InLeft && isInc) return Math.Min(Left(x => func(x) <= value, min, max), Right(x => func(x) >= value, min, max));
+            if (st == SearchType.InRight && isInc) return Math.Max(Left(x => func(x) <= value, min, max), Right(x => func(x) >= value, min, max));
+            if (st == SearchType.OutRight && isInc) return Right(x => func(x) > value, min, max);
+            if (st == SearchType.OutLeft && !isInc) return Left(x => func(x) > value, min, max);
+            if (st == SearchType.InLeft && !isInc) return Math.Min(Left(x => func(x) >= value, min, max), Right(x => func(x) <= value, min, max));
+            if (st == SearchType.InRight && !isInc) return Math.Max(Left(x => func(x) >= value, min, max), Right(x => func(x) <= value, min, max));
+            if (st == SearchType.OutRight && !isInc) return Right(x => func(x) < value, min, max);
+
+            throw new Exception();
+        }
+
+        static int ArraySearch(SearchType st, __long__[] array, __long__ value)
+        {
+            if (array.Length == 0) throw new ArgumentException($"{nameof(array)}.Length > 0");
+
+            var first = array.First();
+            var last = array.Last();
+            var isInc = first <= last;
+
+            var isOutOfRangeToTheLeft = false;
+            if (isInc && st == SearchType.OutLeft) isOutOfRangeToTheLeft = first >= value;
+            if (isInc && st != SearchType.OutLeft) isOutOfRangeToTheLeft = first > value;
+            if (!isInc && st == SearchType.OutLeft) isOutOfRangeToTheLeft = first <= value;
+            if (!isInc && st != SearchType.OutLeft) isOutOfRangeToTheLeft = first < value;
+            if (isOutOfRangeToTheLeft) return -1;
+
+            var isOutOfRangeToTheRight = false;
+            if (isInc && st == SearchType.OutRight) isOutOfRangeToTheRight = last <= value;
+            if (isInc && st != SearchType.OutRight) isOutOfRangeToTheRight = last < value;
+            if (!isInc && st == SearchType.OutRight) isOutOfRangeToTheRight = last >= value;
+            if (!isInc && st != SearchType.OutRight) isOutOfRangeToTheRight = last > value;
+            if (isOutOfRangeToTheRight) return array.Length;
+
+            return (int)Search(st, x => array[x], value, 0, array.Length - 1);
+        }
+
+        public static __int__ OutLeft(Func<__int__, __long__> func, __long__ value, __int__ min, __int__ max)
+        {
+            return Search(SearchType.OutLeft, func, value, min, max);
+        }
+
+        public static __int__ InLeft(Func<__int__, __long__> func, __long__ value, __int__ min, __int__ max)
+        {
+            return Search(SearchType.InLeft, func, value, min, max);
+        }
+
+        public static __int__ InRight(Func<__int__, __long__> func, __long__ value, __int__ min, __int__ max)
+        {
+            return Search(SearchType.InRight, func, value, min, max);
+        }
+
+        public static __int__ OutRight(Func<__int__, __long__> func, __long__ value, __int__ min, __int__ max)
+        {
+            return Search(SearchType.OutRight, func, value, min, max);
+        }
+
+        public static __int__ Range(Func<__int__, __long__> func, __long__ leftValue, __long__ rightValue, __int__ min, __int__ max)
+        {
+            var minIndex = InLeft(func, leftValue, min, max);
+            var maxIndex = InRight(func, rightValue, min, max);
+            if (func(min) < func(max) && func(minIndex) < leftValue) minIndex++;
+            if (func(max) < func(min) && func(minIndex) > leftValue) minIndex++;
+            if (func(min) < func(max) && func(maxIndex) > rightValue) maxIndex--;
+            if (func(max) < func(min) && func(maxIndex) < rightValue) maxIndex--;
+            return Math.Max(0, maxIndex - minIndex + 1);
+        }
+
+        public static int OutLeft(this __long__[] source, __long__ value)
+        {
+            return ArraySearch(SearchType.OutLeft, source, value);
+        }
+
+        public static int InLeft(this __long__[] source, __long__ value)
+        {
+            return ArraySearch(SearchType.InLeft, source, value);
+        }
+
+        public static int InRight(this __long__[] source, __long__ value)
+        {
+            return ArraySearch(SearchType.InRight, source, value);
+        }
+
+        public static int OutRight(this __long__[] source, __long__ value)
+        {
+            return ArraySearch(SearchType.OutRight, source, value);
+        }
+
+        public static int Range(this __long__[] source, __long__ leftValue, __long__ rightValue)
+        {
+            var minIndex = source.InLeft(leftValue);
+            var maxIndex = source.InRight(rightValue);
+            if (minIndex == -1 || (minIndex != source.Length && source[minIndex] != leftValue)) minIndex++;
+            if (maxIndex == source.Length || (maxIndex != -1 && source[maxIndex] != rightValue)) maxIndex--;
+            return Math.Max(0, maxIndex - minIndex + 1);
+        }
+    }
+    //$bs
+
+    public static partial class BinarySearch
+    {
+        public static __int__ MeguruSearch(Func<int, bool> pred, __int__ inValue, __int__ outValue)
+        {
+            while (true)
+            {
+                if (Math.Abs(inValue - outValue) <= 1) return inValue;
+                var mid = (inValue + outValue) / 2;
+                if (pred(mid)) inValue = mid;
+                else outValue = mid;
+            }
+        }
+    }
+}
