4cc64bbfd7485ac9a9a476ae921c12c9974eeec2
AreTrash <tinpuny44@gmail.com>
2019-01-23 22:50:30 +0900

    feat: millerRabin

---
+++ CompetitiveProgrammingSnippets.Test/Algorithm/MathX/MillerRabinTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.Numerics;
+using Xunit;
+
+namespace Algorithm.MathX
+{
+    public class MillerRabinTest
+    {
+        [Fact]
+        void Prime()
+        {
+            Assert.True(MillerRabin.IsPrime(2));
+            Assert.True(MillerRabin.IsPrime(3));
+            Assert.True(MillerRabin.IsPrime(11));
+            Assert.True(MillerRabin.IsPrime(100003));
+            Assert.True(MillerRabin.IsPrime(679067));
+            Assert.True(MillerRabin.IsPrime(1000000007));
+            Assert.True(MillerRabin.IsPrime(1000000000000000003));
+
+            var e64 = "10000000000000000000000000000000000000000000000000000000000000057";
+            Assert.True(MillerRabin.IsPrime(BigInteger.Parse(e64)));
+
+            //素数大富豪
+            var primeMillionaire = "99998888777766665555444433332222131313131313121212121111111011010101111";
+            Assert.True(MillerRabin.IsPrime(BigInteger.Parse(primeMillionaire)));
+
+            //メルセンヌ素数
+            var mersennePrime12th = BigInteger.Pow(2, 127) - 1;
+            Assert.True(MillerRabin.IsPrime(mersennePrime12th));
+        }
+
+        [Fact]
+        void NotPrime()
+        {
+            Assert.False(MillerRabin.IsPrime(0));
+            Assert.False(MillerRabin.IsPrime(1));
+            Assert.False(MillerRabin.IsPrime(57));
+            Assert.False(MillerRabin.IsPrime(3215031751));
+        }
+
+        /*
+        [Fact]
+        void Performance()
+        {
+            const long Min = 0;
+            const long Max = 10000000;
+            var so = new Stopwatch();
+            
+            so.Start();
+            var prime =  new Prime();
+            var pc1 = 0;
+            for (var i = Min; i <= Max; i++)
+            {
+                if (prime.Is(i)) pc1++;
+            }
+            so.Stop();
+            Console.WriteLine(so.ElapsedMilliseconds);
+
+            so.Reset();
+            so.Start();
+            var pc2 = 0;
+            for (var i = Min; i <= Max; i++)
+            {
+                if (MillerRabin.IsPrime(i)) pc2++;
+            }
+            so.Stop();
+            Console.WriteLine(so.ElapsedMilliseconds);
+            Assert.Equal(pc1, pc2);
+        }
+        */
+    }
+}
---
+++ CompetitiveProgrammingSnippets/Algorithm/MathX/MillerRabin.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Algorithm.MathX
+{
+    //$millerRabin
+    public static class MillerRabin
+    {
+        const int RandomTestCount = 30;
+
+        static readonly BigInteger[] BasePrimes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,};
+        static readonly BigInteger[] WitnessRanges =
+        {
+            2047,
+            1373653,
+            25326001,
+            118670087467,//3215031751が例外
+            2152302898747,
+            3474749660383,
+            341550071728321,
+            341550071728321,
+            3825123056546413051,
+            3825123056546413051,
+            3825123056546413051,
+            BigInteger.Parse("318665857834031151167461"),
+            BigInteger.Parse("3317044064679887385961981"), 
+        };
+
+        public static bool IsPrime(BigInteger n)
+        {
+            if (n < 2 || n == 3215031751) return false;
+            if (n < 42) return BasePrimes.Contains(n);
+            if (BasePrimes.Any(p => n % p == 0)) return false;
+
+            var d = n - 1;
+            while ((d & 1) == 0) d >>= 1;
+
+            for (var i = 0; i < BasePrimes.Length; i++)
+            {
+                if (!MillerRabinTest(n, d, BasePrimes[i])) return false;
+                if (n < WitnessRanges[i]) return true;
+            }
+
+            //n > 3.3e24
+            var random = new Random();
+            for (var i = 0; i < RandomTestCount; i++)
+            {
+                if (!MillerRabinTest(n, d, random.Next(1, int.MaxValue))) return false;
+            }
+            return true;
+        }
+
+        static bool MillerRabinTest(BigInteger n, BigInteger d, BigInteger a)
+        {
+            a = BigInteger.ModPow(a, d, n);
+            if (a == 1 || a == n - 1) return true;
+
+            while ((d <<= 1) < n - 1)
+            {
+                a = a * a % n;
+                if (a == n - 1) return true;
+            }
+
+            return false;
+        }
+    }
+    //$millerRabin
+}
