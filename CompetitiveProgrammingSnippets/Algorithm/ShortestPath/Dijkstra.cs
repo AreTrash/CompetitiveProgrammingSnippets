2c14ae12a00bf13f297424799622783bc6241c31
AreTrash <tinpuny44@gmail.com>
2019-02-02 07:58:13 +0900

    fix: dijk - floatとかも考えるとやっぱりパラメーター化しとかないとだめそう

--- CompetitiveProgrammingSnippets/Algorithm/ShortestPath/Dijkstra.cs
+++ CompetitiveProgrammingSnippets/Algorithm/ShortestPath/Dijkstra.cs
@@ -4,23 +4,25 @@ using Algorithm.BasicDataStructure;
 
 namespace Algorithm.ShortestPath
 {
+    using __long__ = Int64;
+
     //参考: プログラミングコンテストチャレンジブック（蟻本）P.97
     //$dijk
     //@Dijkstra O((V+E)LogV) 頂点の数が多く、辺の数が割と少ないとき dependency: pq
     public class Dijkstra
     {
-        public static readonly long Infinity = long.MaxValue;
+        public static readonly __long__ Infinity = __long__.MaxValue;
 
         public struct Edge
         {
             public int To;
-            public long Cost;
+            public __long__ Cost;
         }
 
         public struct Node : IComparable<Node>
         {
             public int Id;
-            public long Cost;
+            public __long__ Cost;
 
             public int CompareTo(Node other)
             {
@@ -36,21 +38,21 @@ namespace Algorithm.ShortestPath
             for (var i = 0; i < v; i++) edges[i] = new List<Edge>();
         }
 
-        public void AddEdge(int from, int to, long cost)
+        public void AddEdge(int from, int to, __long__ cost)
         {
             edges[from].Add(new Edge {To = to, Cost = cost});
         }
 
-        public void AddUndirectedEdge(int v1, int v2, long cost)
+        public void AddUndirectedEdge(int v1, int v2, __long__ cost)
         {
             AddEdge(v1, v2, cost);
             AddEdge(v2, v1, cost);
         }
 
-        public long[] GetDistances(int start)
+        public __long__[] GetDistances(int start)
         {
             var v = edges.Length;
-            var dist = new long[v];
+            var dist = new __long__[v];
 
             for (var i = 0; i < v; i++) dist[i] = Infinity;
             dist[start] = 0;
--- CompetitiveProgrammingSnippets/Algorithm/ShortestPath/DijkstraByMatrix.cs
+++ CompetitiveProgrammingSnippets/Algorithm/ShortestPath/DijkstraByMatrix.cs
@@ -4,17 +4,19 @@ using System.Linq;
 
 namespace Algorithm.ShortestPath
 {
+    using __long__ = Int64;
+
     //参考: プログラミングコンテストチャレンジブック（蟻本）P.96
     //$dijkmat
     //@DijkstraByMatrix O(V^2) 辺の数がものすごく多く、頂点数が割と少ないとき
     public class DijkstraByMatrix
     {
-        public static readonly long Infinity = long.MaxValue;
-        readonly long[,] costMatrix;
+        public static readonly __long__ Infinity = __long__.MaxValue;
+        readonly __long__[,] costMatrix;
 
         public DijkstraByMatrix(int v)
         {
-            costMatrix = new long[v, v];
+            costMatrix = new __long__[v, v];
 
             for (var i = 0; i < v; i++)
             for (var j = 0; j < v; j++)
@@ -23,22 +25,22 @@ namespace Algorithm.ShortestPath
             }
         }
 
-        public void AddEdge(int from, int to, long cost)
+        public void AddEdge(int from, int to, __long__ cost)
         {
             costMatrix[from, to] = cost;
         }
 
-        public void AddUndirectedEdge(int v1, int v2, long cost)
+        public void AddUndirectedEdge(int v1, int v2, __long__ cost)
         {
             AddEdge(v1, v2, cost);
             AddEdge(v2, v1, cost);
         }
 
-        public long[] GetDistances(int start)
+        public __long__[] GetDistances(int start)
         {
             var v = costMatrix.GetLength(0);
             var willUse = new HashSet<int>(Enumerable.Range(0, v));
-            var dist = new long[v];
+            var dist = new __long__[v];
 
             for (var i = 0; i < v; i++) dist[i] = Infinity;
             dist[start] = 0;
